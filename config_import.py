5077bb7db5db68155cace01de4a5d1294cbea0b7
kowloon-dev <dev@kowloonet.org>
2016-04-13 11:29:26 +0900

    web_scraping updated.

--- config_import.py
+++ config_import.py
@@ -1,5 +1,36 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# filename: 
-# summary:  
-# version:  
+
+import configparser
+import logging
+import traceback
+
+# Reading config file
+try:
+    config = configparser.ConfigParser()
+    config.read("config.ini")
+except:
+    logging.error(traceback.format_exc())
+    raise
+
+# ------------  Import parameters from config file  ------------
+
+# [GetWebsite]
+url =             config.get('GetWebsite', 'url')
+title_tag =       config.get('GetWebsite', 'title_tag')
+title_class =     config.get('GetWebsite', 'title_class')
+title =           config.get('GetWebsite', 'title')
+retry_sleep_min = int(config.get('GetWebsite', 'retry_sleep_min'))
+retry_sleep_max = int(config.get('GetWebsite', 'retry_sleep_max'))
+retry_max =       int(config.get('GetWebsite', 'retry_max'))
+result_file =           config.get('GetWebsite', 'result_file')
+
+# [Logging]
+logging_level = config.get('Logging', 'logging_level')
+log_filename =  config.get('Logging', 'log_filename')
+
+
+# ------------  Construct Flask Parameters  ------------
+
+flask_static_path =    "/static"
+flask_templates_path = "/templates"
--- web_scraping.py
+++ web_scraping.py
@@ -1,5 +1,77 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# filename: 
-# summary:  
-# version:  
+
+import config_import as ci
+import log_control
+import requests
+import time
+from bs4 import BeautifulSoup
+import random
+import traceback
+
+
+class GetWebsite:
+
+    def __init__(self):
+        try:
+            self.url = ci.url
+            self.title_tag = ci.title_tag
+            self.title_class = ci.title_class
+            self.title = ci.title
+            self.retry_sleep_min = ci.retry_sleep_min
+            self.retry_sleep_max = ci.retry_sleep_max
+            self.retry_max = ci.retry_max
+            self.result_file = ci.result_file
+        except:
+            err = "Read config failed.\n"
+            log_control.logging.error(err + traceback.format_exc())
+            raise
+
+    def get_website(self):
+
+        retry_count = 0
+        while True:
+            # GET Website
+            get_result = requests.get(self.url)
+
+            # Change encoding
+            # "ISO-8859-1"(default) to "UTF-8"
+            get_result.encoding = 'UTF-8'
+
+            # If the status code is "200" or retry_count has reached "retry_max",
+            # come out of this loop.
+            if get_result.status_code == 200:
+                log_control.logging.debug(self.url + " GET succeeded.(STATUS CODE= " + str(get_result.status_code) + ")")
+                break
+            elif retry_count >= self.retry_max:
+                log_control.logging.error(self.url + " It has reached retry_max. Give up getting website... ")
+                break
+
+            log_control.logging.debug(self.url + " GET failed.(STATUS CODE= " + str(get_result.status_code) + "). Waiting for retry...")
+
+            # Add 1 to "retry_count", empty "get_result", and sleep in random seconds.
+            retry_count += 1
+            get_result = ""
+            time.sleep(random.randint(self.retry_sleep_min,self.retry_sleep_max))
+
+        # Parse the html code.
+        soup = BeautifulSoup(get_result.text, "html.parser")
+        # Find the title class.
+        scraped_code = soup.findAll(self.title_tag, class_=self.title_class)
+
+        print(scraped_code)
+
+        # Seek textline which contains title.
+        for line in scraped_code:
+            if self.title in line:
+                title_check_result = 1
+                break
+            else:
+                title_check_result = 0
+
+        # degug
+        print(self.title)
+        print(title_check_result)
+
+gw = GetWebsite()
+gw.get_website()
