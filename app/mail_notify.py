a8138dfd87e07bfee962ccf5caadd29ab67bd0f5
kowloon-dev <dev@kowloonet.org>
2016-04-18 12:40:28 +0900

    Split config_mail from config.ini.

--- app/mail_notify.py
+++ app/mail_notify.py
@@ -1,24 +1,34 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+from os import pardir
+from os.path import dirname
+from os.path import sep
+import logging
 import traceback
 import requests
-import time
-import random
-import config_import as ci
 import log_control
 import smtplib
 from email.mime.text import MIMEText
+
+import configparser
+
+# Construct config_file path & read config file
+try:
+    pardir_path = dirname(__file__) + sep + pardir
+    config_file = pardir_path + "/config/config_mail.ini"
+    config = configparser.ConfigParser()
+    mailconfig = config.read(config_file)
+except:
+    logging.error(traceback.format_exc())
+    raise
 
 
 class CheckCarried:
 
     def __init__(self):
         try:
-            self.api_url = ci.api_url
-            self.retry_sleep_min = ci.retry_sleep_min
-            self.retry_sleep_max = ci.retry_sleep_max
-            self.retry_max = ci.retry_max
+            self.api_url = mailconfig.api_url
         except:
             err = "Read config failed.\n"
             log_control.logging.error(err + traceback.format_exc())
@@ -27,33 +37,19 @@ class CheckCarried:
 
     def get_api(self):
 
-        retry_count = 0
+        # GET API
+        self.get_result = requests.get(self.api_url)
 
-        while True:
-            # GET API
-            self.get_result = requests.get(self.api_url)
-
-            # If the status code is "200" or retry_count has reached "retry_max",
-            # come out of this loop.
-            if self.get_result.status_code == 200:
-                log_control.logging.debug(
-                    self.api_url + " GET API succeeded.(STATUS CODE= " + str(self.get_result.status_code) + ")")
-                break
-            elif retry_count >= self.retry_max:
-                log_control.logging.error(
-                    self.api_url + " It has reached retry_max. Give up getting API... ")
-                break
-
+        # Verify the status code.
+        if self.get_result.status_code == 200:
+            log_control.logging.debug(
+                self.api_url + " GET API succeeded.(STATUS CODE= " + str(self.get_result.status_code) + ")")
+        else:
             log_control.logging.debug(self.api_url + " GET API failed.(STATUS CODE= " + str(
-                self.get_result.status_code) + "). Waiting for retry...")
-
-            # Add 1 to "retry_count", empty "get_result", and sleep in random seconds.
-            retry_count += 1
-            self.get_result = ""
-            time.sleep(random.randint(self.retry_sleep_min, self.retry_sleep_max))
+                self.get_result.status_code) + ").")
 
 
-    def check_carried(self):
+    def check(self):
 
         # Parse the response with json()
         json_result = self.get_result.json()
@@ -77,14 +73,14 @@ class CheckCarried:
 class MailSend:
     def __init__(self):
         try:
-            self.smtp_host = ci.smtp_host
-            self.smtp_port = ci.smtp_port
-            self.local_host = ci.local_host
-            self.auth_id = ci.smtpauth_id
-            self.auth_pass = ci.smtpauth_pass
-            self.from_addr = ci.from_addr
-            self.to_addr = ci.to_addr
-            self.mail_title = ci.mail_title
+            self.smtp_host = mailconfig.smtp_host
+            self.smtp_port = mailconfig.smtp_port
+            self.local_host = mailconfig.local_host
+            self.auth_id = mailconfig.smtpauth_id
+            self.auth_pass = mailconfig.smtpauth_pass
+            self.from_addr = mailconfig.from_addr
+            self.to_addr = mailconfig.to_addr
+            self.mail_title = mailconfig.mail_title
         except:
             err = "Read config failed.\n"
             log_control.logging.error(err + traceback.format_exc())
@@ -106,13 +102,13 @@ class MailSend:
         except:
             smtp.quit()
             log_control.logging.error(
-                "Sending Email has failed. ")
+                "Sending mail has failed. ")
             return
 
 
 # Create Instance and execute functions
 mn = CheckCarried()
 mn.get_api()
-mn.check_carried()
+mn.check()
 
 exit()
